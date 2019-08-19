9c1eb72564319b986073b2747120caeebc78756a
Jesswin W Varghese <jesswinw@gmail.com>
2019-08-19 23:22:58 +0530

    Greeting the user with taking his name as input

---
+++ greet.py
@@ -0,0 +1,7 @@
+#This programs says hello and asks for a name and greets the user
+
+print("Hello,There")
+
+Uname = input("Please,Enter your sweet Name:") #taking input(user name) from the user 
+
+print("It's nice to meet you,"+ Uname) #printing the greeting output
