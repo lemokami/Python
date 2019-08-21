91726a02cb19183675912efda20d26f41d630cb8
Jesswin W Varghese <jesswinw@gmail.com>
2019-08-21 22:41:27 +0530

    a user defined function

---
+++ doubleNumwithFunc.py
@@ -0,0 +1,7 @@
+def doubleNum(num): #function syntax -> def <function_name> (<argument>)
+    
+    return (num*2) #function return (function ends itself if encountered by a return statement)
+
+numb = int(input("Enter the number:")) #takes in the number
+
+print(doubleNum(numb)) # calling the function as well as printing the return value
