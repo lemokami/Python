9551a334dfb9340a5774715a3fd78bb693c84608
Jesswin W Varghese <jesswinw@gmail.com>
2019-08-23 23:04:14 +0530

    Finds the reverse of a given string using string slicing

---
+++ revString.py
@@ -0,0 +1,6 @@
+print("Find the reverse of a string\n") #finds the reverse of a given string
+
+word = input("Enter the string:") #taking in the string
+
+print(word[::-1]) #using slicing to find the reverse of the string
+print() #printing a newline
