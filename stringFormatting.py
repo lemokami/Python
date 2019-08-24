facc2b36f40724fe6e3b14473a5c8bb64d38ed94
Jesswin W Varghese <jesswinw@gmail.com>
2019-08-24 18:59:49 +0530

    uses string formatting for printing

---
+++ stringFormatting.py
@@ -0,0 +1,6 @@
+#this program uses fstrings to format the strings
+name = input("Enter your name:")
+
+age = int(input("Enter your age:"))
+
+print(f"Hello,{name} you are {(age+1)} next year") #printing the strings using formatted string literals
