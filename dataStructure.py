010730038191cc1079d1111224efc4dda98a6e48
Jesswin W Varghese <jesswinw@gmail.com>
2019-08-24 18:59:26 +0530

    uses python data structure(here  a list of dictionaries)

---
+++ dataStructure.py
@@ -0,0 +1,10 @@
+import pprint as pp #importing a module for pretty printing
+
+#Below is a data structure which is a list of dictinaries
+dstruct = [{'name':'Maximilian','course':'javaScript','rating':5.0},
+           {'name':'Maximilian','course':'Angular','rating':5.0},
+           {'name':'Maximilian','course':'React','rating':4.7},
+           {'name':'Jose Portila','course':'Python','rating':4.8}]
+
+
+pp.pprint(dstruct) #pretty printing the data-structure
