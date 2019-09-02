4f38bf23fd9ae1981b862df16e630987dad636f6
jesswinw <jesswinw@gmail.com>
2019-09-02 20:50:56 +0530

    accessing and traversing through xlsx files

---
+++ funWithExcel.py
@@ -0,0 +1,18 @@
+#opens an excel file and trverse through the contents and display it better to know the no of rows and columns
+import openpyxl as op
+#openpycl is a python library for managing xlsx files
+
+#use sample file inn Files\\sampleExcel.xlsx
+book = op.load_workbook("Files\\sampleExcel.xlsx")#opening the file and storing the handle in the var 
+#for linux use / instead of \\ 
+
+names = book.sheetnames #.sheetnames returns the sheetnames as a list 
+
+sheet = book[names[0]] #takes the first sheet
+
+
+for i in range(1,44): #loop to traverse through the sheet
+    print(f"{i} ",end='')
+    for j in range(1,8):
+        print(f"{sheet.cell(row=i,column=j).value}\t",end='') #printing the sheet in the format
+    print()
