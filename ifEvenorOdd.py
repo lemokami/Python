5dbc352f11bef378fffddd621388c8789dc674ff
jesswinw <jesswinw@gmail.com>
2019-09-02 21:16:12 +0530

    added some comments

--- ifEvenorOdd.py
+++ ifEvenorOdd.py
@@ -1,7 +1,7 @@
 #checks if a given number is even or not 
 num = int(input("Enter your number:")) #input takes a string value so it must be casted to int
 
-if(num % 2 == 0):
+if(num % 2 == 0): #if number / 2 gives a remainder 0 it is even else odd
     print("Even")
 else:
     print("odd")
