3e61f11deead0b0b023b1e43dd9b396547f1569c
jesswinw <jesswinw@gmail.com>
2019-09-02 20:53:52 +0530

    Added comments

--- Gsearch.py
+++ Gsearch.py
@@ -1,11 +1,12 @@
-import webbrowser as wb
-import sys
+#a simple program to take a keyword as argument and opening the result page for the keyword in google search
+import webbrowser as wb #webbrowser to access the web browser
+import sys #sys to take the keyword given as argument if any
 
 args = sys.argv
 
-if(len(args)>1):
+if(len(args)>1): #if there's a keyword passed through command line argument
     search = ''.join(args[1:])
     wb.open("https://www.google.com/search?&q="+search)
-else:
-    search = input("Search on google:")
+else: 
+    search = input("Search on google:") #else takes the keyword by asking the user
     wb.open("https://www.google.com/search?&q="+search)
